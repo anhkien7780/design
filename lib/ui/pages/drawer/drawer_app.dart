e8ca219763ce033796228384ca03355a1df63327
Nguyễn Văn Kiên <nguyenvankien@Admins-Mac-mini.local>
2025-07-29 14:53:45 +0700

    feat: open and close drawer button

--- lib/ui/pages/drawer/drawer_app.dart
+++ lib/ui/pages/drawer/drawer_app.dart
@@ -15,19 +15,27 @@ class DrawerApp extends StatelessWidget {
               decoration: BoxDecoration(color: Colors.blue),
               child: Text("Drawer Header"),
             ),
+            ListTile(title: const Text("Item 1"), onTap: () {}),
+            ListTile(title: const Text("Item 2"), onTap: () {}),
             ListTile(
-              title: const Text("Item 1"),
-              onTap: (){},
-            ),
-            ListTile(
-              title: const Text("Item 2"),
-              onTap: (){},
-            ),
-            ListTile(
-              title: const Text("Item 3"),
-              onTap: (){},
+              title: const Text("Close drawer"),
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
           ],
+        ),
+      ),
+      body: Center(
+        child: Builder(
+          builder: (context) {
+            return OutlinedButton(
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              child: Text("Open drawer"),
+            );
+          }
         ),
       ),
     );
